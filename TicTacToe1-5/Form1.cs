667a2458c09397fc50cb2738f64f584c92f4cd16
Danilo Madrigalejos <dan@yahoo.com>
2022-06-04 19:10:48 +0800

    Hehehe gumagana na ung X ska O

--- TicTacToe1-5/Form1.cs
+++ TicTacToe1-5/Form1.cs
@@ -21,46 +21,118 @@ namespace TicTacToe1_5
         private void btn1_Click(object sender, EventArgs e)
         {
             btn1.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
             btn2.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
             btn3.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
             btn4.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
             btn5.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
             btn6.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
             btn7.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
             btn8.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
             btn9.Text = turn.ToString();
+            if (turn == 'X')
+            {
+                turn = 'O';
+            }
+            else
+            {
+                turn = 'X';
+            }
         }
     }
 }
