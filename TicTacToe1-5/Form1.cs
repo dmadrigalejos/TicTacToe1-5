20d0f1108e4c9d530b7f657f6a5dff8462912e99
Danilo Madrigalejos <dan@yahoo.com>
2022-06-04 19:03:30 +0800

    Gumagana na ang X hehehe

--- TicTacToe1-5/Form1.cs
+++ TicTacToe1-5/Form1.cs
@@ -12,9 +12,55 @@ namespace TicTacToe1_5
 {
     public partial class Form1 : Form
     {
+        char turn = 'X';
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btn1.Text = turn.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btn2.Text = turn.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btn3.Text = turn.ToString();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            btn4.Text = turn.ToString();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            btn5.Text = turn.ToString();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            btn6.Text = turn.ToString();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btn7.Text = turn.ToString();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            btn8.Text = turn.ToString();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            btn9.Text = turn.ToString();
         }
     }
 }
